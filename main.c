9d745acc3e83ab8252eda5ca63d1716812fefe35
Kawaharasouta <kawahara6514@gmail.com>
2018-05-28 23:38:31 +0900

    save

--- main.c
+++ main.c
@@ -9,13 +9,26 @@ void *thread_f(void *arg);
 int main(int argc, char **argv){
 	pthread_t thread;
 
-	if (pthread_create(&thread, NULL, thread_f, (void *)5) != 0){
+	if (argc != 2){
+		fprintf(stderr, "arg error\n");
+		exit(1);
+	}
+
+	if (pthread_create(&thread, NULL, thread_f, (void *)atoi(argv[1])) != 0){
 		fprintf(stderr, "pthread_create error\n");
 		exit(1);
 	}
 
-	printf("main\n");
-	sleep(2);
+	printf("main\ncreate thread %u\n", thread);
+	//sleep(2);
+
+	int ret;
+	if (pthread_join(thread, (void **)&ret) != 0){
+		fprintf(stderr, "pthread_join error\n");
+		exit(1);
+	}
+	thread = NULL;
+	printf("ret: %d\n", ret);
 	printf("main fin\n");
 
 	return 0;
@@ -29,7 +42,7 @@ void *thread_f(void *arg){
 		printf("%d times new thread\n", i);
 		if (i == n)
 #if 1
-			pthread_exit(NULL);
+			pthread_exit((void *)n);
 #else
 			exit(1);
 #endif
