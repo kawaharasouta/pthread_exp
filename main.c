a02c4a69f37a36601be8cdf407b6ab3a1cb70ac8
Kawaharasouta <kawahara6514@gmail.com>
2018-05-28 23:08:27 +0900

    save

--- main.c
+++ main.c
@@ -4,23 +4,35 @@
 
 #include<pthread.h>
 
-void th(void *arg);
+void *thread_f(void *arg);
 
 int main(int argc, char **argv){
 	pthread_t thread;
 
-	if (pthread_create(&thread, NULL, th, NULL) != 0){
+	if (pthread_create(&thread, NULL, thread_f, (void *)5) != 0){
 		fprintf(stderr, "pthread_create error\n");
 		exit(1);
 	}
-	printf("main fin\n");
 
-	sleep(1);
+	printf("main\n");
+	sleep(2);
+	printf("main fin\n");
 
 	return 0;
 }
 
-void th(void *arg){
-	printf("new thread\n");
+void *thread_f(void *arg){
+	int i;
+	int n = (int)arg;
+
+	for (i = 0; i < n; i++){
+		printf("%d times new thread\n", i);
+		if (i == n)
+#if 1
+			pthread_exit(NULL);
+#else
+			exit(1);
+#endif
+	}
 }
 
